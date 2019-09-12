e6d63621485399f47a52b5916638fd386be8a5eb
“Jayson <jaysoncoppo@gmail.com>
2019-09-12 02:59:37 +0000

    initial

---
+++ image-blur1.rb
@@ -0,0 +1,26 @@
+class Image
+  attr_reader :array2d
+
+  def initialize(array2d)
+    @array2d = array2d
+  end
+
+  def output_image
+    @array2d.each do |row|
+      str = ""
+      row.each do |digit|
+        str << digit.to_s
+      end
+      puts str
+    end
+  end
+
+end
+
+image = Image.new([
+  [0, 0, 0, 0, 1, 2],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
